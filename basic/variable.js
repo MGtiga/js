3c50a3d478bf95175f2fc6d55127aa0cacb4469f
MGtiga <124690718+MGtiga@users.noreply.github.com>
2024-06-12 14:22:11 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 14553
+let accountEmail = "karankshetri321@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId= 2
+accountEmail= "herkf@gmail.com"
+accountPassword="wie3"
+accountCity ="pokhara"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail, accountId,accountPassword,accountCity])
