312fad3df4e8cac7f92e2ed2f1c2cd8224de4e3d
MGtiga <124690718+MGtiga@users.noreply.github.com>
2024-06-12 14:21:43 +0000

    datatypes

---
+++ basic/2datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat js code as newer version 
+// alert(3+3) // we are usign node js , not browser
+//datatypes
+//number
+//bigInt
+//string
+//boolean
+//undefined
+//null
+//symbol
+
+
+//object
+
+console.log(typeof "helllo"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
