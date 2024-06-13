c9c82417165e1cb3066ae40d20ddced09ca416bd
MGtiga <124690718+MGtiga@users.noreply.github.com>
2024-06-13 16:58:37 +0000

    conversion

---
+++ basic/3conversion.js
@@ -0,0 +1,25 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let s="33"
+
+console.log(typeof s);
+console.log(typeof (s));
+
+let valueInNum = Number(s)
+console.log( typeof valueInNum);
+
+let p= true
+console.log(typeof p);
+valueInNumP = Number(p)
+console.log(typeof valueInNumP);
+console.log(valueInNumP);
+
+let l= null
+console.log(typeof l)
+
+let isLoggedIn =1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
